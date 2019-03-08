da37680eac315aa42763c5513e3fe847a3a266e2
Ryan Davis <epikit@gmail.com>
2019-03-07 20:24:40 -0600

    added 'empty' method

--- pm/Strings.hx
+++ pm/Strings.hx
@@ -6,6 +6,10 @@ class Strings {
     public static inline function has(s:String, sub:String):Bool {
         return s.indexOf(sub) != -1;
     }
+
+    public static inline function empty(s: String):Bool {
+        return s == null || s.length == 0;
+    }
 }
 
 typedef Strs = StringTools;
