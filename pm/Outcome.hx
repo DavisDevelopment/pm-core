b9e013726205705e89d65f9faaaee6b0ea19fb8c
ryan <epikit@gmail.com>
2019-05-23 16:00:21 -0500

    added some fancy new methods to pm.Outcome

--- pm/Outcome.hx
+++ pm/Outcome.hx
@@ -1,5 +1,9 @@
 package pm;
 
+import haxe.ds.Option;
+
+using pm.Options;
+
 @:using(pm.Outcome.Outcomes)
 enum Outcome<Result, Error> {
     Success(result: Result);
@@ -10,14 +14,14 @@ class Outcomes {
     /**
       check if the given Outcome is a success
      **/
-    public static inline function isSuccess<T, Err>(o:Outcome<T, Err>):Bool {
+    public static function isSuccess<T, Err>(o:Outcome<T, Err>):Bool {
         return o.match(Success(_));
     }
 
     /**
       check if the given Outcome is a failure
      **/
-    public static inline function isFailure<T, Err>(o:Outcome<T, Err>):Bool {
+    public static function isFailure<T, Err>(o:Outcome<T, Err>):Bool {
         return o.match(Failure(_));
     }
 
@@ -25,13 +29,30 @@ class Outcomes {
       if [o] is a success Outcome, return its value
       else if [o] is a failure Outcome, throw its value
      **/
-    public static inline function manifest<T, Err>(o: Outcome<T, Err>):T {
+    public static function manifest<T, Err>(o: Outcome<T, Err>):T {
         switch ( o ) {
             case Success( value ): 
                 return value;
 
             case Failure( error ):
                 throw error;
+        }
+    }
+    public static inline function sure<D,F>(o: Outcome<D, F>):D {
+        return manifest( o );
+    }
+
+    public static function toOption<D, F>(o: Outcome<D, F>):Option<D> {
+        return switch o {
+            case Success(x): Some(x);
+            case Failure(_): None;
+        }
+    }
+
+    public static function flatMap<D1,F1,D2>(o:Outcome<D1,F1>, f:D1->Outcome<D2, F1>):Outcome<D2, F1> {
+        return switch o {
+            case Success(res): f(res);
+            case Failure(x): Failure(x);
         }
     }
 }
