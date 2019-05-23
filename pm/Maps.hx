70488bbdf3509a327b446274f45d0d7ba71938e0
ryan <epikit@gmail.com>
2019-05-23 16:00:55 -0500

    fixed issues with pm.Maps

--- pm/Maps.hx
+++ pm/Maps.hx
@@ -9,14 +9,19 @@ using pm.Functions;
 class Maps {
     @:generic
     public static function clone<K, V>(m: IMap<K, V>, ck:K->K, cv:V->V):IMap<K, V> {
-        return [for (k=>v in m) ck(k)=>cv(v)];
+        return cast [for (k=>v in m) ck(k)=>cv(v)];
     }
 
     @:generic
     public static function map<K1, V1, K2, V2>(m: IMap<K1, V1>, pred:Pair<K1, V1>->Pair<K2, V2>):IMap<K2, V2> {
-        return [for (k=>v in m) {
+        return cast [for (k=>v in m) {
             var p = pred(new Pair(k, v));
             p.left => p.right;
         }];
+    }
+
+    @:generic
+    public static function keyArray<K, V>(m:IMap<K, V>):Array<K> {
+        return m.keys().array();
     }
 }
