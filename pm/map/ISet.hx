a69ab3fb1d897ef77f60e9b2ccb2a98c62738016
ryan <epikit@gmail.com>
2019-10-17 16:12:05 -0500

    created pm.map.OrderedMap and pm.map.Set types

--- pm/map/ISet.hx
+++ pm/map/ISet.hx
@@ -24,3 +24,33 @@ typedef SetObject<T> = {
 	function iterator():Iterator<T>;
 	function toArray():Array<T>;
 }
+
+interface ISet2<T> {
+    public var length(get, null):Int;
+    
+    public function exists(k:T):Bool;
+    public function remove(k:T):Bool;
+    public function iterator():Iterator<T>;
+    public function toString():String;
+    public function toArray():Array<T>;
+    
+    public function add(v:T):Bool;
+    public function addMany(seq:Seq<T>):Void;
+    public function clear():Void;
+    public function clone():Set<T>;
+    
+    
+    public function subset(other:Set<T>):Bool;
+    public function properSubset(other:Set<T>):Bool;
+    public function equals(other:Set<T>):Bool;
+    public function disjoint(other:Set<T>):Bool;
+    public function compareTo(other:Set<T>):Int;
+    
+    public function union(other:Set<T>):Set<T>;
+    public function intersect(other:Set<T>):Set<T>;
+    public function difference(other:Set<T>):Set<T>;
+    public function exclude(other:Set<T>):Set<T>;
+    
+    public function cartesianProduct<U>(other:Set<U>):Set<Pair<T,U>>;
+}
+
--- pm/map/OrderedMap.hx
+++ pm/map/OrderedMap.hx
@@ -1,11 +1,20 @@
 package pm.map;
 
+import pm.iterators.EmptyIterator;
+import haxe.Constraints.IMap;
 import haxe.ds.BalancedTree.TreeNode;
+
+import pm.iterators.*;
 
 /**
  * Modified from BalancedTree
+ * [TODO]
+ *  - refactor iteration methods to use Generator<?>, once finished
  */
-class OrderedMap<K, V> {
+#if yield
+@:yield
+#end
+class OrderedMap<K, V> implements IMap<K, V> {
 	public var length(default, null):Int;
 
 	private var root: TreeNode<K, V>;
@@ -16,9 +25,9 @@ class OrderedMap<K, V> {
 		`cmp` is a function that compares two keys.
 		When `cmp` is not specified, it defaults to Reflect.compare.
 	**/
-	public function new(?cmp:K->K->Int) {
+	public function new(?cmp : #if haxe4 (a:K, b:K)->Int #else K->K->Int #end) {
 		length = 0;
-		compare = cmp == null ? Reflect.compare : cmp;
+		compare = cmp == null ? ((a:K, b:K)->pm.Arch.compareThings(a, b)) : cmp;
 	}
 
 	/**
@@ -98,6 +107,26 @@ class OrderedMap<K, V> {
 		return ret.iterator();
 	}
 
+	public function nodes():Iterator<TreeNode<K, V>> { 
+		var ret = [];
+		nodeIteratorLoop(root, ret);
+		return ret.length == 0 ? new EmptyIterator() : ret.iterator();
+	}
+
+	public function keyValueIterator() {
+		return inline new MappedIterator(nodes(), function(node) return {key:node.key, value:node.value});
+	}
+
+	public function keys() {
+		return inline new MappedIterator(nodes(), function(node) return node.key);
+	}
+
+	public inline function clear() {
+		root.dispose();
+		root = null;
+		length = 0;
+	}
+
 	function setLoop(k:K, v:V, node:TreeNode<K, V>) {
 		if (node == null)
 			return new TreeNode<K, V>(null, k, v, null);
@@ -134,6 +163,33 @@ class OrderedMap<K, V> {
 			iteratorLoop(node.left, acc);
 			acc.push(node.value);
 			iteratorLoop(node.right, acc);
+		}
+	}
+
+	function nodeIteratorLoop(node:TreeNode<K, V>, acc:Array<TreeNode<K, V>>) {
+		if (node != null) {
+			nodeIteratorLoop(node.left, acc);
+			acc.push(node);
+			nodeIteratorLoop(node.right, acc);
+		}
+	}
+
+	public inline function copy() {
+		var m = new OrderedMap(this.compare);
+		m.root = deepCopyNode(this.root);
+		m.length = this.length;
+		return m;
+	}
+
+	inline function deepCopyNode(node: TreeNode<K, V>) {
+		return switch node {
+			case null: null;
+			case _: new TreeNode(
+				deepCopyNode(node.left),
+				node.key,
+				node.value,
+				deepCopyNode(node.right)
+			);
 		}
 	}
 
@@ -183,4 +239,156 @@ class OrderedMap<K, V> {
 	public function toString() {
 		return root == null ? '{}' : '{${root.toString()}}';
 	}
+}
+
+/**
+	A tree node of `haxe.ds.BalancedTree`.
+**/
+private class TreeNode<K, V> {
+	@:native('l') 
+	public var left : TreeNode<K, V>;
+	@:native('r') 
+	public var right : TreeNode<K, V>;
+	@:native('k') 
+	public var key : K;
+	@:native('v') 
+	public var value : V;
+
+	@:native('lH')
+	#if as3
+	public
+	#end
+	var _height : Int;
+
+	public function new(l, k, v, r, h = -1) {
+		left = l;
+		key = k;
+		value = v;
+		right = r;
+		if (h == -1)
+			_height = (left.get_height() > right.get_height() ? left.get_height() : right.get_height()) + 1;
+		else
+			_height = h;
+	}
+
+	extern public inline function get_height():Int
+		return this == null ? 0 : _height;
+
+	extern public inline function dispose() {
+		if (this != null) {
+			left.dispose();
+			left = null;
+			right.dispose();
+			right = null;
+			key = null;
+			value = null;
+			_height = -1;
+		}
+	}
+
+	public function toString() {
+		return (left == null ? "" : left.toString() + ", ") + '$key=$value' + (right == null ? "" : ", " + right.toString());
+	}
+}
+
+class TreeWalker<K, V> {
+	var node:TreeNode<K, V>;
+
+	public function new(root) {
+		this.node = root;
+	}
+	public function compute():TreeStep<K, V> {
+		if (node == null) {
+			return End;
+		}
+		else {
+			var steps = [End, End, End];
+			if (node.left != null)
+				steps[0] = Walk(new TreeWalker(node.left));
+			steps[1] = Yield(node);
+			if (node.right != null)
+				steps[2] = Walk(new TreeWalker(node.right));
+			return switch steps {
+				case [End, self, End]: self;
+				case [left, self, End]: Lnk(left, self);
+				case [End, self, right]: Lnk(self, right);
+				case [left, self, right]: Lnk(left, Lnk(self, right));
+				default:
+					throw new Error('Invalid: ${steps}');
+			}
+		}
+	}
+
+	public function pack() {
+		var currentIter:Iterator<TreeNode<K, V>> = cast new EmptyIterator();
+		var currentStep = End;
+		var stack = new LinkedStack();
+		var completion = {};
+		
+		function inner_next() {
+			switch currentStep {
+				case End:
+					currentIter = null;
+
+				case Yield(node):
+					currentIter = new SingleIterator(node);
+					currentStep = End;
+
+				case Walk(walker):
+					currentIter = walker.get().pack();
+					currentStep = End;
+
+				case Lnk(current, next):
+					stack.push(next.get());
+					currentIter = Std.is(currentIter, EmptyIterator)?currentIter:new EmptyIterator();
+					currentStep = current;
+			}
+		}
+
+		function outer_next():TreeNode<K, V> {
+			while (true) {
+				if (currentIter == null && currentStep.match(End)) {
+					// throw new Error('Invalid call; iteration has finished');
+					if (stack.isEmpty()) {
+						throw new Error('Invalid call; iteration is complete');
+					}
+					else {
+						currentStep = stack.pop();
+						inner_next();
+					}
+				}
+				else if (currentIter.hasNext()) {
+					return currentIter.next();
+				}
+				else {
+					inner_next();
+					continue;
+				}
+			}
+		}
+
+		var isComplete = false;
+		function hasNext():Bool {
+			if (isComplete) return false;
+			else if (!(currentIter == null && currentStep.match(End) && stack.isEmpty())) {
+				return currentIter.hasNext();
+			}
+			else {
+				isComplete = true;
+				return false;
+			}
+		}
+
+		return {
+			hasNext: hasNext,
+			next: outer_next
+		};
+	}
+}
+
+enum TreeStep<K,V> {
+	End;
+	Yield(node: TreeNode<K, V>);
+	Walk(walker: Lazy<TreeWalker<K, V>>);
+	Lnk(current:TreeStep<K, V>, next:Lazy<TreeStep<K, V>>);
 }
