efcc14a6c631ffa096df064777609fa1de5b335d
Ryan Davis <epikit@gmail.com>
2020-01-05 21:58:34 -0600

    Macro-generation of assertion errors
    Did some cajiggery to pm.Assert to allow for macro-based auto-generation of error messages

--- pm/Assert.hx
+++ pm/Assert.hx
@@ -4,21 +4,46 @@ import pm.Lazy;
 
 import haxe.PosInfos;
 
+#if macro
+import haxe.macro.Expr;
+using haxe.macro.ExprTools;
+#end
+
 class Assert {
-    /**
-      throws an error if [condition] is not met
-     **/
-    public static inline function assert(condition:Bool, ?error:Dynamic, ?pos:PosInfos):Void {
-        #if (debug || keep_assertions)
+    public static inline function assert(condition:Bool, ?error:Dynamic, ?pos:#if !macro PosInfos #else Dynamic #end):Void {
         if ( !condition )
             _toss(error, pos);
-        #end
+    }
+    
+    /**
+     utility method for throwing an exception
+     **/
+     @:noCompletion
+     public static function _toss(?error:Dynamic, ?pos:#if !macro PosInfos #else Dynamic #end) {
+        if (error == null) {
+            throw new AssertionFailureError(error, pos);
+        }
+        else {
+                throw error;
+        }
     }
 
+    #if !macro
     /**
-      throws an error if [fn] doesn't cause an error to be thrown
+     throws an error if [condition] is not met
      **/
-    public static inline function assertThrows<E>(fn:Void->Void, ?msg:Lazy<E>, ?pos:PosInfos):Void {
+    
+    public static inline function assertDebug(condition:Bool, ?error:Dynamic, ?pos:PosInfos) {
+		#if (debug || keep_assertions)
+		if (!condition)
+			_toss(error, pos);
+		#end
+    }
+    
+    /**
+     throws an error if [fn] doesn't cause an error to be thrown
+     **/
+     public static inline function assertThrows<E>(fn:Void->Void, ?msg:Lazy<E>, ?pos:PosInfos):Void {
         #if (debug || keep_assertions)
         try {
             fn();
@@ -29,18 +54,39 @@ class Assert {
         }
         #end
     }
+    
+#else
 
-    /**
-      utility method for throwing an exception
-     **/
-    private static function _toss(?error:Dynamic, ?pos:PosInfos) {
-        if (error == null) {
-            throw new AssertionFailureError(error, pos);
+#if !display
+    public static function massert(condition:Expr, rest:Array<Expr>):ExprOf<Bool> {
+        var error:Expr = macro null;
+        switch rest {
+            case []:
+                var txt = try condition.toString() catch (e: Dynamic) null;
+                if (txt != null) {
+                    error = macro $v{'AssertionFailed: $txt'};
+                }
+            
+            case [err]:
+                error = err;
+
+            default:
         }
-        else {
-            throw error;
-        }
+        return macro (@:mergeBlock {
+            var b = $condition;
+            if (!b) {
+                pm.Assert._toss($error);
+            }
+            b;
+        }) == true;
+    }
+#end
+
+#end
+
+    public static #if !macro macro #end function aassert(condition #if macro , rest:Array<Expr> #end) {
+        return #if macro massert(condition, rest) #else assert(condition) #end;
     }
 }
-
+    
 class AssertionFailureError extends Error {}
